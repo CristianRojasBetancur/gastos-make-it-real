ec45f7f2708fb5a37910f60c91a397b0218cfe20
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-07 17:31:52 -0500

    Filtrando gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,16 @@
 class ExpensesController < ApplicationController
   def index
-   @expenses = Expense.order("date DESC")
+    p params[:concept]
+    p params[:category_id]
+    if params[:concept].nil? || params[:category_id].nil?
+      @expenses = Expense.order("date DESC")
+    else
+      @expenses = Expense.where("concept LIKE ? AND category_id LIKE ?",
+      "%#{params[:concept]}%",
+      "%#{params[:category_id]}%"
+      ).order("date DESC")
+    end
   end
 end
+
+# Book.where("title LIKE ?", params[:title] + "%")
