86400366fa5abcc08c8f37de34f07d9df70c1e82
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-08 23:41:25 -0500

    Gastos por usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,12 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
     p params[:concept]
     p params[:category_id]
     if params[:concept].nil? || params[:category_id].nil?
-      @expenses = Expense.order("date DESC")
+      @expenses = Expense.where(user_id: current_user.id).order("date DESC")
     else
-      @expenses = Expense.where("concept LIKE ? AND category_id LIKE ?",
+      @expenses = Expense.where(user_id: current_user.id).where("concept LIKE ? AND category_id LIKE ?",
       "%#{params[:concept]}%",
       "%#{params[:category_id]}%"
       ).order("date DESC")
