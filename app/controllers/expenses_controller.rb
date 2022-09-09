475dda488c8c219d53262549d1b404fb282f5f75
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-08 23:57:07 -0500

    Gastos por usuario v2

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,17 +1,13 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
   def index
-    p params[:concept]
-    p params[:category_id]
-    if params[:concept].nil? || params[:category_id].nil?
-      @expenses = Expense.where(user_id: current_user.id).order("date DESC")
-    else
-      @expenses = Expense.where(user_id: current_user.id).where("concept LIKE ? AND category_id LIKE ?",
+    @expenses = Expense.where(user_id: current_user.id).order("date DESC")
+    unless params[:concept].nil? || params[:category_id].nil?
+      @expenses = Expense.where("user_id = ? AND concept LIKE ? AND category_id LIKE ?",
+      current_user.id,
       "%#{params[:concept]}%",
       "%#{params[:category_id]}%"
       ).order("date DESC")
     end
   end
 end
-
-# Book.where("title LIKE ?", params[:title] + "%")
